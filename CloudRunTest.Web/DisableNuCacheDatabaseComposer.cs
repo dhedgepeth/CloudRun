572191acc91c7eddff117119d3886dfc5062e7a7
dhedgepeth <dnh@umbraco.com>
2024-12-10 10:43:03 -0500

    Load balance

---
+++ CloudRunTest.Web/DisableNuCacheDatabaseComposer.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Core.Composing;
+
+namespace CloudRunTest.Web
+{
+    public class DisableNuCacheDatabaseComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            var settings = new Umbraco.Cms.Infrastructure.PublishedCache.PublishedSnapshotServiceOptions
+            {
+                IgnoreLocalDb = true
+            };
+            builder.Services.AddSingleton(settings);
+        }
+    }
+}
---
+++ CloudRunTest.Web/ServerRoleAccessors/SingleServerRoleAccessor.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Core.Sync;
+
+namespace CloudRunTest.Web.ServerRoleAccessors
+{
+    public sealed class SingleServerRoleAccessor : IServerRoleAccessor
+    {
+        public ServerRole CurrentServerRole => ServerRole.Single;
+    }
+
+}
